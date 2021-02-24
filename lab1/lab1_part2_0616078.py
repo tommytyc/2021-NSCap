f909c88da07f6437fc0741aaf84d4187113add55
tommytyc <tommytyc.cs06@nctu.edu.tw>
2021-02-24 18:19:51 +0800

    init commit lab1

---
+++ lab1/lab1_part2_0616078.py
@@ -0,0 +1,37 @@
+import time
+from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.node import Node, Switch
+from mininet.cli import CLI
+
+def topology():
+    net = Mininet()
+
+    # add
+    h1 = net.addHost("h1")
+    h2 = net.addHost("h2")
+    h3 = net.addHost("h3")
+    h4 = net.addHost("h4")
+
+    s1 = net.addSwitch("s1", failMode = 'standalone')
+    s2 = net.addSwitch("s2", failMode = 'standalone')
+    s3 = net.addSwitch("s3", failMode = 'standalone')
+    s4 = net.addSwitch("s4", failMode = 'standalone')
+
+    # link
+    net.addLink("s1", "s2")
+    net.addLink("s1", "s4")
+    net.addLink("s3", "s2")
+    net.addLink("s3", "s4")
+
+    net.addLink("h1", "s1")
+    net.addLink("h2", "s1")
+    net.addLink("h3", "s3")
+    net.addLink("h4", "s3")
+
+    net.start()
+    CLI(net)
+    net.stop()
+
+if __name__ == "__main__":
+    topology()
